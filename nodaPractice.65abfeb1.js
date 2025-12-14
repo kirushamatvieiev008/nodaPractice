36104122b0732f866a9a163add82ed7e4747f04d
kirushamatvieiev008 <mkiril087@gmail.com>
2025-12-14 15:08:30 +0200

    Updates

---
+++ nodaPractice.65abfeb1.js
@@ -0,0 +1,2 @@
+console.log(3),console.log(((o=21)=>{let e="",l=crypto.getRandomValues(new Uint8Array(o|=0));for(;o--;)e+="useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict"[63&l[o]];return e})(5));
+//# sourceMappingURL=nodaPractice.65abfeb1.js.map
